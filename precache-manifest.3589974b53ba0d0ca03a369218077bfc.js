f2f4b91f6ec496e64dc0c674d8dc0542544a52bd
Bikash888 <bikashdulal150@gmail.com>
2020-01-05 13:10:23 +0545

    Updates

---
+++ precache-manifest.3589974b53ba0d0ca03a369218077bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865b00ca80a1b0def9249cf49e539ca0",
+    "url": "/BurgerProjectReact/index.html"
+  },
+  {
+    "revision": "300448a48802313b5d10",
+    "url": "/BurgerProjectReact/static/css/main.89975961.chunk.css"
+  },
+  {
+    "revision": "7a4c8d1b0c012314e14e",
+    "url": "/BurgerProjectReact/static/js/2.350f273f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BurgerProjectReact/static/js/2.350f273f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "300448a48802313b5d10",
+    "url": "/BurgerProjectReact/static/js/main.0ae5070e.chunk.js"
+  },
+  {
+    "revision": "e3bcaf1906142a0cae0f",
+    "url": "/BurgerProjectReact/static/js/runtime-main.c9f5a0a6.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerProjectReact/static/media/burgerLogo.b8503d26.png"
+  }
+]);
